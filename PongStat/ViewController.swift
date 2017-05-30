0d148cffd80c816569d034bd2e3c147a4715ccab
Cowboy Lynk <cowboylynk@icloud.com>
2017-05-29 23:33:36 -0400

    Added Initial project

--- PongStat/ViewController.swift
+++ PongStat/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Variables
+    var madeCounter = 1
+    var removedCounter = 1
+    
+    // Outlets
+    @IBOutlet weak var made: UILabel!
+    @IBOutlet weak var removed: UILabel!
+    @IBOutlet weak var cup1: UIButton!
+    
+    // Actions
+    func normalTap(_ sender: UIGestureRecognizer){
+        made.text = String(madeCounter)
+        madeCounter += 1
+    }
+    
+    func longTap(_ sender: UIGestureRecognizer){
+        if sender.state == .began {
+            removed.text = String(removedCounter)
+            removedCounter += 1
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Adds tap gestures to buttons
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(normalTap(_:)))
+        let longGesture = UILongPressGestureRecognizer(target: self, action: #selector(longTap(_:)))
+        tapGesture.numberOfTapsRequired = 1
+        cup1.addGestureRecognizer(tapGesture)
+        cup1.addGestureRecognizer(longGesture)
     }
 
     override func didReceiveMemoryWarning() {
