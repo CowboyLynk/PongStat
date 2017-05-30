7ba59dced45e69f28b3d8f10cbb90acfc44fee48
Cowboy Lynk <cowboylynk@icloud.com>
2017-05-30 10:56:16 -0400

    Added basic UI

--- PongStat/ViewController.swift
+++ PongStat/ViewController.swift
@@ -12,13 +12,21 @@ class ViewController: UIViewController {
     // Variables
     var madeCounter = 1
     var removedCounter = 1
+    var missedCounter = 1
     
     // Outlets
     @IBOutlet weak var made: UILabel!
     @IBOutlet weak var removed: UILabel!
     @IBOutlet weak var cup1: UIButton!
+    @IBOutlet weak var missedButton: UIButton!
     
-    // Actions
+    
+    //Actions
+    @IBAction func missed(_ sender: Any) {
+        missedButton.setTitle("MISSED: \(missedCounter)", for: .normal)
+        missedCounter += 1
+    }
+    
     func normalTap(_ sender: UIGestureRecognizer){
         made.text = String(madeCounter)
         madeCounter += 1
@@ -33,6 +41,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Custon missed button appearance
+        missedButton.layer.shadowColor = UIColor(red:0.80, green:0.20, blue:0.10, alpha:1.0).cgColor
+        missedButton.layer.shadowOpacity = 1
+        missedButton.layer.shadowRadius = 0
+        missedButton.layer.shadowOffset = CGSize(width: 0, height: 5)
+        missedButton.layer.cornerRadius = 15
         
         // Adds tap gestures to buttons
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(normalTap(_:)))
