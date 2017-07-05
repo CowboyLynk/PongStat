afc4b551923c9bfa11d432f28e1f11269a328a6f
Cowboy Lynk <cowboylynk@icloud.com>
2017-07-05 23:20:55 +0200

    Updated instructions page

--- PongStat/InstructionsPageViewController.swift
+++ PongStat/InstructionsPageViewController.swift
@@ -8,10 +8,99 @@
 
 import UIKit
 
-class InstructionsPageViewController: UIPageViewController {
+class InstructionsPageViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    // Variables
+    var pageControl = UIPageControl()
+    
+    // Functions
+    func newVc(viewController: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: viewController)
+    }
 
+    lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newVc(viewController: "MadeCup"), self.newVc(viewController: "PartnerMadeCup"), self.newVc(viewController: "ReRack"),
+                self.newVc(viewController: "Undo"), self.newVc(viewController: "ScoreCalc")]
+    }()
+    
+    // MARK: Data source functions.
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        // User is on the first view controller and swiped left to loop to
+        // the last view controller.
+        guard previousIndex >= 0 else {
+            //return orderedViewControllers.last
+            // Uncommment the line below, remove the line above if you don't want the page control to loop.
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        // User is on the last view controller and swiped right to loop to
+        // the first view controller.
+        guard orderedViewControllersCount != nextIndex else {
+            //return orderedViewControllers.first
+            // Uncommment the line below, remove the line above if you don't want the page control to loop.
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    func configurePageControl() {
+        // The total number of pages that are available is based on how many available colors we have.
+        pageControl = UIPageControl(frame: CGRect(x: 0,y: self.view.bounds.height - 120, width: self.view.bounds.width, height: 50))
+        self.pageControl.numberOfPages = orderedViewControllers.count
+        self.pageControl.currentPage = 0
+        self.pageControl.tintColor = UIColor.white
+        self.pageControl.pageIndicatorTintColor = UIColor.gray
+        self.pageControl.currentPageIndicatorTintColor = UIColor.white
+        self.view.addSubview(pageControl)
+    }
+    
+    // MARK: Delegate functions
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let pageContentViewController = pageViewController.viewControllers![0]
+        self.pageControl.currentPage = orderedViewControllers.index(of: pageContentViewController)!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(red:0.36, green:0.64, blue:0.48, alpha:1.0)
+        
+        self.delegate = self
+        configurePageControl()
+        self.dataSource = self
+        
+        // This sets up the first view that will show up on our page control
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
