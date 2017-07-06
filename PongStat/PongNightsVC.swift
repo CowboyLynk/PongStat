fb63475ca0f25a3f1e28f1bc2772244fe7f79878
Cowboy Lynk <cowboylynk@icloud.com>
2017-07-06 18:47:49 +0200

    Added ability to delete a night

--- PongStat/PongNightsVC.swift
+++ PongStat/PongNightsVC.swift
@@ -80,26 +80,38 @@ class PongNightsVC: UIViewController {
     func clearNights(){
         let alert = UIAlertController(title: "Reset all night graphs?", message: "Are you sure that you want to erase all night scores?", preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.default, handler: nil))
-        alert.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.destructive, handler: { action in
+        alert.addAction(UIAlertAction(title: "Reset", style: UIAlertActionStyle.destructive, handler: { action in
             UserDefaults.standard.set(nil, forKey: "PongNight")
+            self.viewDidLoad()
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    func deleteChartButtonPressed(sender: UIButton){
+        let alert = UIAlertController(title: "Delete this night?", message: "Deleting a night is permanent and you will not be able to get back these scores.", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Delete", style: UIAlertActionStyle.destructive, handler: { action in
+            var allNightCharts = UserDefaults.standard.array(forKey: "PongNight")!
+            allNightCharts.remove(at: sender.tag)
+            UserDefaults.standard.set(allNightCharts, forKey: "PongNight")
             self.viewDidLoad()
         }))
         self.present(alert, animated: true, completion: nil)
     }
 
     override func viewDidLoad() {
-        // Clears the view
-        scrollView.clearView()
-        
         //Adds bar button item
         let restartButton = UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(clearNights))
         tabBarController?.navigationItem.setRightBarButtonItems([restartButton], animated: true)
+        
+        // Clears the view
+        scrollView.clearView()
         
         var yPos = 15
         scrollView.contentSize.height = 0
         
         let nights = (UserDefaults.standard.array(forKey: "PongNight")?.reversed())
         if (nights != nil){
+            var index = (UserDefaults.standard.array(forKey: "PongNight")?.count)! - 1
             noGamesLabel.isHidden = true
             for night in nights!{
                 // Creates the views that display the night's information
@@ -107,6 +119,7 @@ class PongNightsVC: UIViewController {
                 nightChart = LineChartView(frame: CGRect(x: 0, y: 60, width: view.bounds.width, height: 150))
                 let chartInfoView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 75))
                 let chartInfo = UILabel(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 50))
+                let deleteChartButton = UIButton(frame: CGRect(x: view.bounds.width - 60, y: 0, width: 60, height: 50))
                 let background = UIView(frame: CGRect(x: 0, y: 50, width: view.bounds.width, height: 100))
                 
                 // Adds all the scores to the chart
@@ -118,6 +131,9 @@ class PongNightsVC: UIViewController {
                 chartInfo.font = UIFont(name: "HelveticaNeue-Bold", size: 14)
                 chartInfo.textColor = UIColor(red:0.56, green:0.59, blue:0.62, alpha:1.0)
                 chartInfo.text = "NIGHT OF: \(nightDate!)"
+                deleteChartButton.setImage(#imageLiteral(resourceName: "Delete"), for: .normal)
+                deleteChartButton.tag = index
+                deleteChartButton.addTarget(self, action: #selector(deleteChartButtonPressed(sender:)), for: .touchUpInside)
                 chartInfoView.backgroundColor = .white
                 chartInfoView.layer.cornerRadius = 5
                 view.center.x = self.view.center.x
@@ -130,6 +146,7 @@ class PongNightsVC: UIViewController {
                 
                 // Adds all the subviews
                 chartInfoView.addSubview(chartInfo)
+                chartInfoView.addSubview(deleteChartButton)
                 view.addSubview(chartInfoView)
                 view.addSubview(background)
                 view.addSubview(nightChart)
@@ -138,6 +155,7 @@ class PongNightsVC: UIViewController {
                 // Increments certain variables
                 scrollView.contentSize.height += 230
                 yPos += 230
+                index -= 1
                 
             }
         } else {
